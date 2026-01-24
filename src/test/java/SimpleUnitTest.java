37bae3707dd735dbb96fc330e9f7505bd4cb966b
dorsaf kraydi <dkraydi@somezzo.com>
2026-01-24 17:46:53 +0100

    fix bug

---
+++ src/test/java/SimpleUnitTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleUnitTest {
+
+    @Test
+    void testAddition() {
+        int a = 5;
+        int b = 3;
+        assertEquals(8, a + b);
+    }
+}
