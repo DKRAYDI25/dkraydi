14a0ab11c0ef3369cf43438fb467c178faa1c72e
dorsaf kraydi <dkraydi@somezzo.com>
2026-01-29 19:16:45 +0100

    add test called method

--- src/test/java/tn/esprit/studentmanagement/StudentServiceTest.java
+++ src/test/java/tn/esprit/studentmanagement/StudentServiceTest.java
@@ -9,14 +9,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class StudentServiceTest {
 
     @Test
-    void testWithMockito() {
+    void testGetStudentName() {
 
+        // Mock du service
         StudentService serviceMock = Mockito.mock(StudentService.class);
 
+        // Comportement simulé
         Mockito.when(serviceMock.getStudentName()).thenReturn("Dorsaf");
 
+        // Appel
         String result = serviceMock.getStudentName();
 
+        // Vérification
         assertEquals("Dorsaf", result);
     }
+
+
+    @Test
+    void testMethodIsCalled() {
+
+        // Mock du service
+        StudentService serviceMock = Mockito.mock(StudentService.class);
+
+        // Appel de la méthode
+        serviceMock.getStudentNamee();
+
+        // Vérifier que la méthode a bien été appelée une fois
+        Mockito.verify(serviceMock, Mockito.times(1)).getStudentNamee();
+    }
+
+
 }
