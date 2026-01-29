da0aed2f0cff3a391ddf6a66cceec9c5f32e6939
dorsaf kraydi <dkraydi@somezzo.com>
2026-01-29 19:07:52 +0100

    add test

--- src/test/java/tn/esprit/studentmanagement/StudentServiceTest.java
+++ src/test/java/tn/esprit/studentmanagement/StudentServiceTest.java
@@ -1,2 +1,22 @@
-package tn.esprit.studentmanagement;public class StudentServiceTest {
+package tn.esprit.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import tn.esprit.studentmanagement.services.StudentService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StudentServiceTest {
+
+    @Test
+    void testWithMockito() {
+
+        StudentService serviceMock = Mockito.mock(StudentService.class);
+
+        Mockito.when(serviceMock.getStudentName()).thenReturn("Dorsaf");
+
+        String result = serviceMock.getStudentName();
+
+        assertEquals("Dorsaf", result);
+    }
 }
